8a480bda2f802e9214204eb35f7f592925e28a17
Patrick Harrison <jahman@bellatlantic.net>
2019-07-29 14:47:34 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,6 +1,21 @@
 
-# require 'pry
-|name| "Hi, #{name}" }
+# require 'pry'
+
+
+
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#   binding.pry
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+
+# hello(["Tim", "Tom", "Jim"]) { 
+#   |name| "Hi, #{name}" }
   
 def hello(array)
   i = 0
@@ -8,12 +23,12 @@ def hello(array)
   while i < array.length
     collection << yield(array[i])
     i += 1
-
   end
 
   collection
 end
 
+end
 
 require 'pry'
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
